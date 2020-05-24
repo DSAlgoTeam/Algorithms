75ab6314626fe3b5f8c1d2a6f84ab0ad2bdb5cae
Avinash Rath <avinash.rrath@gmail.com>
2020-05-24 22:28:32 +0530

    Assignment 1 - Python (#2)
    * Add linear_search and binary_search
    
    * Added LIFO and FIFO using linked lists
    
    * Update .gitignore
    
    * Updated linked list LIFO and FIFO
    
    * Added array LIFO and FIFO
    
    * Fixed errors in search
    
    * LIFO and FIFO now inherit from LinkedList
    
    * LIFO FIFO list implementation
    
    * import abstractmethod from abc
    
    * FIFO and LIFO linked lists implementation
    
    * Fixed __str__() method
    
    * Removed old implementation of linked lists
    
    * Fixed __add__() method in Node
    
    * Update .gitignore
    
    * Updated Binary search
    
    * Removed __init__/py
    
    * Refactor
    
    * Updated List_Common.__str__()

---
+++ Python/Algorithms/search/search.py
@@ -0,0 +1,55 @@
+from typing import List, TypeVar
+
+
+def linear_search(element, lst):
+    '''
+    Searches for the given element in the given list using `Linear Search` algorithm,
+    and returns the index of the element in the list. If the element is not found, returns -1
+    
+    params:
+        element: Element that needs to be search for in the given `lst`
+        lst(list): List of elements in which the `element` needs to be searched for.
+
+    returns:
+        index(int): index of `element` in `lst` if found, else -1
+    '''
+    for index in range(0,len(lst)):
+        if lst[index] == element:
+            return index
+    return -1
+
+
+def binary_search(element, lst):
+    '''
+    Searches for the given element in the given list using `Linear Search` algorithm,
+    and returns the index of the element in the list. If the element is not found, returns -1
+    
+    params:
+        element: Element that needs to be search for in the given `lst`
+        lst(list): List of elements in which the `element` needs to be searched for.
+
+    returns:
+        index(int): index of `element` in `lst` if found, else -1
+    '''
+
+    if lst != sorted(lst):
+        raise ValueError(' param ( lst )  must be sorted.')
+
+
+    lstLength = len(lst)
+
+    lo, hi = 0, lstLength - 1
+    
+    while lo <= hi:
+        mid = (hi + lo) // 2
+
+        if lst[mid] == element:
+            return mid
+        
+        if lst[mid] >= element:
+            hi = mid - 1
+        
+        else:
+            lo = mid + 1
+    
+    return -1
