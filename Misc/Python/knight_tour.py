1f397bbf6eacbeeec76525842c2a6e37f40f621f
Avinash Rath <avinash.rrath@gmail.com>
2020-07-05 19:11:57 +0530

    Started knight tour

---
+++ Misc/Python/knight_tour.py
@@ -0,0 +1,9 @@
+'''
+backtracking
+'''
+def knight_tour(n, startx, starty):
+    '''
+    n: grid dimension
+    startx: starting position of x (from 0 to n-1)
+    starty: starting position of y (from o to n-1)
+    '''
